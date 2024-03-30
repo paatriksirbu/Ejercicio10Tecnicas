aab862449bc71c0b400fc6315543c577ffd48519
Patrik <psirb@myuax.com>
2024-03-30 17:35:07 +0100

    Clase libro con metodos para añadir al stock, eliminar y consultar.

--- src/main/java/ControlStockLibreria/Libro.java
+++ src/main/java/ControlStockLibreria/Libro.java
@@ -1,4 +1,36 @@
 package ControlStockLibreria;
+import java.util.*;
 
 public class Libro {
+
+    private String nombreLibro;
+    private ArrayList<Integer> stock;
+
+    public Libro(String nombreLibro){
+        this.nombreLibro = nombreLibro;
+        this.stock = new ArrayList<Integer>();
+    }
+
+    public void addToStock(int cantidad) throws ExcepcionStockDesbordado{
+        int stockActual = 0;
+        if (Integer.MAX_VALUE - stockActual < cantidad){
+            throw new ExcepcionStockDesbordado(this.nombreLibro);
+        }
+        int nuevoStock = stockActual + cantidad;
+        this.stock.add(nuevoStock);
+    }
+
+    public void removeFromStock(int cantidad) throws ExcepcionStockDesbordado{
+        int stockActual = this.stock.get(this.stock.size() - 1);
+        if (cantidad > stockActual){
+            throw new ExcepcionStockDesbordado(this.nombreLibro);
+        }
+        int nuevoStock = stockActual - cantidad;
+        this.stock.add(nuevoStock);
+    }
+
+    public int getStock(){
+        return this.stock.get(this.stock.size() - 1);
+    }
+
 }
