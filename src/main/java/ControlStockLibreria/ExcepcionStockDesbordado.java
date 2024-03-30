bc9256916bf3af56f7b7075334f9ea880f3005ab
Patrik <psirb@myuax.com>
2024-03-30 17:24:42 +0100

    Excepcion del Stock desbordado, nos dira que el stock del libro excede la capacidad del sistema.

--- src/main/java/ControlStockLibreria/ExcepcionStockDesbordado.java
+++ src/main/java/ControlStockLibreria/ExcepcionStockDesbordado.java
@@ -1,4 +1,8 @@
 package ControlStockLibreria;
 
-public class ExcepcionStockDesbordado {
+public class ExcepcionStockDesbordado  extends Exception{
+
+    public ExcepcionStockDesbordado(String nombreLibro){
+        super("El stock del libro" + nombreLibro + " excede la capacidad de este sistema.");
+    }
 }
--- src/main/java/ControlStockLibreria/SistemaControlStock.java
+++ src/main/java/ControlStockLibreria/SistemaControlStock.java
@@ -1,4 +1,12 @@
 package ControlStockLibreria;
 
+
 public class SistemaControlStock {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de control de stock de libros");
+
+
+
+    }
 }
